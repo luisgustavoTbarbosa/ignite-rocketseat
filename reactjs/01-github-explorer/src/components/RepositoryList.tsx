537cd8f8ea26f023f5159b605e88f771c78f38e0
Luis Gustavo <gustavobarbosag7@gmail.com>
2022-05-24 22:26:29 -0300

    init dtmoney project

--- reactjs/01-github-explorer/src/components/RepositoryList.tsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.tsx
@@ -19,6 +19,8 @@ export function RepositoryList() {
         .then(data => setRepositories(data))
     }, []);
 
+    console.log(repositories)
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
