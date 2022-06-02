5dfbf2cd0d29bfd323c63250af919f4f402916de
Luis Gustavo <gustavobarbosag7@gmail.com>
2022-06-01 23:17:02 -0300

    add modal lib from react, create modal compontent and styles

--- reactjs/dtmoney/src/components/Header/index.tsx
+++ reactjs/dtmoney/src/components/Header/index.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg';
+
+
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTrasanctionModal: () => void;
+}
+
+export function Header({ onOpenNewTrasanctionModal }: HeaderProps) {
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dtmoney" />
-                <button type="button" >
+                <button type="button" onClick={onOpenNewTrasanctionModal}>
                     Nova transação
                 </button>
             </Content>
