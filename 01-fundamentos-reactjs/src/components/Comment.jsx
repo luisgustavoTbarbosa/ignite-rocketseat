32e4b3ad9ede4c9dad6dff3c99d4afc24ff167a4
Luis Gustavo <gustavobarbosag7@gmail.com>
2022-08-15 21:07:33 -0300

    finishing the project

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,48 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/luisgustavoTbarbosa.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                <header>
+                    <div className={styles.authorAndTime}>
+                        <strong>Luis Gustavo</strong>
+                        <time title='11 de Maio as 08:13h' dateTime='2022-05-11 08:13:00'>Cerca de 1h atrás</time>
+                    </div>
+
+                    <button onClick={handleDeleteComment} title="Deletar comentário">
+                        <Trash size={24} />
+                    </button>
+                </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
